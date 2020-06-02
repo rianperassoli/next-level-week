601f6d34f046ced82b3b5d1ee182aff8cd4949d7
Rian Perassoli <rian.perassoli@gmail.com>
2020-06-02 20:49:53 -0300

    Criada rota para retornar items com o caminho da imagem pronto

--- server/src/routes.ts
+++ server/src/routes.ts
@@ -1,9 +1,19 @@
 import express from 'express'
+import knex from './database/connection'
 
 const routes = express.Router()
 
-routes.get('/', (request, respose) => {
-    return respose.json({ message: 'Running' })
+routes.get('/items', async (request, response) => {
+  const items = await knex('item').select('*')
+
+  const serializedItems = items.map(item => {
+    return {
+      title: item.title,
+      imageUrl: `http://localhost:3333/uploads/${item.image}`
+    }
+  })
+
+  return response.json(serializedItems)
 })
 
 export default routes
