0f1f6c61914fa05e399d2f77a7a5a4a81ffcde52
Rian Perassoli <rian.perassoli@gmail.com>
2020-06-02 21:12:29 -0300

    Criada rota para adicionar ponto de coleta

--- server/src/routes.ts
+++ server/src/routes.ts
@@ -8,12 +8,31 @@ routes.get('/items', async (request, response) => {
 
   const serializedItems = items.map(item => {
     return {
+      id: item.id,
       title: item.title,
       imageUrl: `http://localhost:3333/uploads/${item.image}`
     }
   })
 
   return response.json(serializedItems)
+})
+
+routes.post('/points', async (request, response) => {
+  const { name, email, whatsapp, latitude, longitude, city, uf, items } = request.body
+
+  const trx = await knex.transaction()
+
+  const insertedIds = await trx('point').insert({ image: 'image-fake', name, email, whatsapp, latitude, longitude, city, uf })
+
+  const point_id = insertedIds[0]
+
+  const pointItems = items.map((item_id: number) => {
+    return { item_id, point_id }
+  })
+
+  await trx('point_item').insert(pointItems)
+
+  return response.json({ success: true })
 })
 
 export default routes
