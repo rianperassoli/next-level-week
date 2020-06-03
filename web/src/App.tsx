8286dbea8995d8bf621bd0975914d1dc8329d4a7
Rian Perassoli <rian.perassoli@gmail.com>
2020-06-03 19:48:15 -0300

    removidas implementações do Header e o componente

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,15 +1,9 @@
 import React from 'react';
 import './App.css';
 
-import Header from './Header'
-
 function App() {
   return (
-    <div>
-      <Header title="Ecoleta" />
-
-      <h1>Hello world</h1>
-    </div>
+    <h1>Hello world</h1>
   )
 }
 
