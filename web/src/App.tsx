91b60d81936e3807a4bb2eee63706bf080ec85db
Rian Perassoli <rian.perassoli@gmail.com>
2020-06-03 19:25:40 -0300

    Criado componente Header

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,10 +1,16 @@
 import React from 'react';
 import './App.css';
 
+import Header from './Header'
+
 function App() {
   return (
-    <h1>Hello world</h1>
-  );
+    <div>
+      <Header />
+
+      <h1>Hello world</h1>
+    </div>
+  )
 }
 
 export default App;
