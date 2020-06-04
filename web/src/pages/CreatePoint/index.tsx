8373756a28a15194ce2216968b837ee6bc262ca7
Rian Perassoli <rian.perassoli@gmail.com>
2020-06-03 23:00:45 -0300

    redirecionando para a home apos salvar item

--- web/src/pages/CreatePoint/index.tsx
+++ web/src/pages/CreatePoint/index.tsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useState, ChangeEvent, FormEvent } from 'react'
-import { Link } from 'react-router-dom'
+import { Link, useHistory } from 'react-router-dom'
 import { FiArrowLeft } from 'react-icons/fi'
 import { Map, TileLayer, Marker } from 'react-leaflet'
 import axios from 'axios'
@@ -40,6 +40,8 @@ const CreatePoint = () => {
     email: '',
     whatsapp: ''
   })
+
+  const history = useHistory()
 
   useEffect(() => {
     navigator.geolocation.getCurrentPosition(position => {
@@ -128,7 +130,9 @@ const CreatePoint = () => {
 
     await api.post('points', data)
 
-    alert('ponto ok')
+    alert('ponto criado')
+
+    history.push('/')
   }
 
   return (
