d7b51f964a32c3bb99326f2e5441e5666949af0b
Rian Perassoli <rian.perassoli@gmail.com>
2020-06-05 18:58:42 -0300

    Ajustado caminho da imagem para funcionar no celular. IP FIXO

--- server/src/controllers/ItemsController.ts
+++ server/src/controllers/ItemsController.ts
@@ -10,7 +10,7 @@ class ItemsController {
       return {
         id: item.id,
         title: item.title,
-        imageUrl: `http://localhost:3333/uploads/${item.image}`
+        imageUrl: `http://192.168.1.14:3333/uploads/${item.image}`
       }
     })
 
