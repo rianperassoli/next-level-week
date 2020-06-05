27ba24a3291f004941a7b8c043818cddd583ee68
Rian Perassoli <rian.perassoli@gmail.com>
2020-06-05 19:25:41 -0300

    Finalizada estrutura da pagina de detalhe

--- mobile/src/pages/Detail/index.tsx
+++ mobile/src/pages/Detail/index.tsx
@@ -1,9 +1,46 @@
 import React from 'react'
-import { View, StyleSheet } from 'react-native'
+import { View, StyleSheet, TouchableOpacity, Image, Text, SafeAreaView } from 'react-native'
+import { useNavigation } from '@react-navigation/native'
+import { Feather as Icon, FontAwesome } from '@expo/vector-icons'
+import { RectButton } from 'react-native-gesture-handler'
 
 const Detail = () => {
+
+  const navigation = useNavigation()
+
+  const handleNavigateBack = () => {
+    navigation.goBack()
+  }
+
   return (
-    <View />
+    <SafeAreaView style={{ flex: 1 }}>
+      <View style={styles.container}>
+        <TouchableOpacity onPress={handleNavigateBack}>
+          <Icon name="arrow-left" size={20} color="#34cb79" />
+        </TouchableOpacity>
+
+        <Image style={styles.pointImage} source={{ uri: 'https://images.unsplash.com/photo-1533900298318-6b8da08a523e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=400&q=50' }} />
+
+        <Text style={styles.pointName}>Mercado do Rian</Text>
+        <Text style={styles.pointItems}>Lampada</Text>
+
+        <View style={styles.address}>
+          <Text style={styles.addressTitle}>Endereço</Text>
+          <Text style={styles.addressContent}>São Miguel do Oeste, SC</Text>
+        </View>
+      </View>
+      <View style={styles.footer}>
+        <RectButton style={styles.button} onPress={() => { }}>
+          <FontAwesome name="whatsapp" size={20} color="#FFF" />
+          <Text style={styles.buttonText}>Whatsapp</Text>
+        </RectButton>
+
+        <RectButton style={styles.button}>
+          <Icon name="mail" size={20} color="#FFF" onPress={() => { }} />
+          <Text style={styles.buttonText}>E-mail</Text>
+        </RectButton>
+      </View>
+    </SafeAreaView>
   )
 }
 
