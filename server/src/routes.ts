e00f788db3f367e0387f6e442ae9d9a3fbed2e1d
Rian Perassoli <rian.perassoli@gmail.com>
2020-06-05 23:03:54 -0300

    configuração do multer para receber a imagem

--- server/src/routes.ts
+++ server/src/routes.ts
@@ -1,8 +1,11 @@
 import express from 'express'
 import PointsController from './controllers/PointsController'
 import ItemsController from './controllers/ItemsController'
+import multer from 'multer'
+import multerConfig from './config/multer'
 
 const routes = express.Router()
+const upload = multer(multerConfig)
 
 const pointsController = new PointsController()
 const itemsController = new ItemsController()
@@ -13,6 +16,7 @@ routes.get('/items', itemsController.index)
 
 routes.get('/points/:id', pointsController.show)
 routes.get('/points', pointsController.index)
-routes.post('/points', pointsController.create)
+
+routes.post('/points', upload.single('image'), pointsController.create)
 
 export default routes
