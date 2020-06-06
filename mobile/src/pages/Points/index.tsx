36142823d39ffbbcd6eaff4216b7d4af3326598c
Rian Perassoli <rian.perassoli@gmail.com>
2020-06-05 21:45:05 -0300

    Passa a receber os dados de estado e cidade para listar os pontos

--- mobile/src/pages/Points/index.tsx
+++ mobile/src/pages/Points/index.tsx
@@ -2,7 +2,7 @@ import React, { useState, useEffect } from 'react'
 import { View, StyleSheet, TouchableOpacity, Text, ScrollView, Image, Alert } from 'react-native'
 import Constants from 'expo-constants'
 import { Feather as Icon } from '@expo/vector-icons'
-import { useNavigation } from '@react-navigation/native'
+import { useNavigation, useRoute } from '@react-navigation/native'
 import MapView, { Marker } from 'react-native-maps'
 import { SvgUri } from 'react-native-svg'
 import api from '../../services/api'
@@ -22,9 +22,17 @@ interface Point {
   longitude: number
 }
 
+interface Params {
+  uf: string
+  city: string
+}
+
 const Points = () => {
 
   const navigation = useNavigation()
+  const route = useRoute()
+
+  const routeParams = route.params as Params
 
   const [items, setItems] = useState<Item[]>([])
   const [points, setPoints] = useState<Point[]>([])
@@ -40,14 +48,14 @@ const Points = () => {
   useEffect(() => {
     api.get('points', {
       params: {
-        city: 'São Miguel do Oeste',
-        uf: 'SC',
-        items: '1,2,3,4,5,6'
+        city: routeParams.city,
+        uf: routeParams.uf,
+        items: selectedItems
       }
     }).then(response => {
       setPoints(response.data)
     })
-  }, [])
+  }, [selectedItems])
 
   useEffect(() => {
     const loadPosition = async () => {
